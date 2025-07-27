8d8424ef1b6562abc7a47960e86048e66bbac958
AllRounderBoy7 <sahanapraveen2006@gmail.com>
2025-07-27 12:29:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,5 +36,6 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 10000;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
